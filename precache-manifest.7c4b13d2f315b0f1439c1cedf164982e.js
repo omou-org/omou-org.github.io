faafad9bfc9ce9aad2f96788258f7f4d06bb3de8
Daniel Huang <danielhuang@g.ucla.edu>
2019-09-13 01:47:01 -0700

    Updates

---
+++ precache-manifest.7c4b13d2f315b0f1439c1cedf164982e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb4f20719603fd61610f95cbc0ffcec5",
+    "url": "/stage-1-demo/index.html"
+  },
+  {
+    "revision": "01ab83ffad1620cd79a9",
+    "url": "/stage-1-demo/static/css/2.d6508308.chunk.css"
+  },
+  {
+    "revision": "5d643a0fd32cd8bf3a23",
+    "url": "/stage-1-demo/static/css/main.52f5bc20.chunk.css"
+  },
+  {
+    "revision": "01ab83ffad1620cd79a9",
+    "url": "/stage-1-demo/static/js/2.f855e517.chunk.js"
+  },
+  {
+    "revision": "5d643a0fd32cd8bf3a23",
+    "url": "/stage-1-demo/static/js/main.ab16f2a0.chunk.js"
+  },
+  {
+    "revision": "321874ec19384c1c269a",
+    "url": "/stage-1-demo/static/js/runtime~main.d28c3fb0.js"
+  },
+  {
+    "revision": "64906079e5020719811b97b020d39aec",
+    "url": "/stage-1-demo/static/media/identifier.64906079.svg"
+  }
+]);
