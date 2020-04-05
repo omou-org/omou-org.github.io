f98d1d8b8192e891b7fcf36f39b174f7cf201fb0
Jeffrey Su <jsflca2012@gmail.com>
2020-04-05 14:23:11 -0700

    commit 1

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,7 +1,31 @@
 import React from "react";
+import Grid from '@material-ui/core/Grid';
+import "./Home.scss";
 
 export default function Home() {
 	return (<div>
-		Home
+		<Grid container className="top">
+			<Grid item md={6} className="topContent">
+				<div className="upper"> 
+				Don’t just digitize the way your tutoring center runs - transform it.
+				</div>
+				<div className="bottom">
+				Omou is a tutoring management service designed for remote-learning.
+				</div>
+			</Grid>
+			<Grid item md={6}>
+				INSERT IMAGE HERE
+			</Grid>
+		</Grid>
+		<Grid container className="middle">
+			<Grid item md={6} className="middleContent">
+				Don’t just digitize the way your tutoring center runs - transform it.
+			</Grid>
+			<Grid item md={6}>
+			</Grid>
+		</Grid>
+		<Grid>
+			3
+		</Grid>
 	</div>)
 }
