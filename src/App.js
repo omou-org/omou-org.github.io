423af3f327e9bd3a50d8e9dab76b820386bde31b
Calvin Fronda <cfronda209@gmail.com>
2020-04-05 18:00:53 -0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import './App.css';
-import {ThemeProvider} from '@material-ui/core/styles';
-import {appBarTheme, theme} from './theme/theme'
-import {AppBar, makeStyles, Toolbar, Typography} from "@material-ui/core";
-import {BrowserRouter as Router, NavLink} from "react-router-dom";
-import {Routes} from "./routes";
-import {primaryColor} from "./theme/colors";
-import Button from "@material-ui/core/esm/Button/Button";
+import { ThemeProvider } from '@material-ui/core/styles';
+import { appBarTheme, theme } from './theme/theme'
+import { AppBar, makeStyles, Toolbar, Typography } from "@material-ui/core";
+import { BrowserRouter as Router, NavLink } from "react-router-dom";
+import { Routes } from "./routes";
+import { primaryColor } from "./theme/colors";
 
-const useStyles = makeStyles((appBarTheme)=>({
-    link:{
-        textDecoration:"none",
-        textDecorationColor:"inherit",
+const useStyles = makeStyles((appBarTheme) => ({
+    link: {
+        textDecoration: "none",
+        textDecorationColor: "inherit",
     },
     grow: { // create gap between left and right components on the app bar
         flexGrow: 1,
     },
     desktopMenu: {
-        display:"flex"
+        display: "flex"
     },
     desktopMenuItem: {
         marginLeft: "10px",
@@ -46,17 +45,17 @@ function NavigationBar() {
         <AppBar
             color="primary"
             position="static">
-            <Toolbar style={{backgroundColor: primaryColor}}>
+            <Toolbar style={{ backgroundColor: primaryColor }}>
                 <NavLink className={classes.link} to="/">
                     <Typography
                         variant="h3">
                         omou
                     </Typography>
                 </NavLink>
-                <div className={classes.grow}/>
+                <div className={classes.grow} />
                 <div className={classes.desktopMenu}>
                     {
-                        MenuItems.map(({title, link}) => (
+                        MenuItems.map(({ title, link }) => (
                             <NavLink
                                 className={classes.link}
                                 to={link}
@@ -79,12 +78,12 @@ function NavigationBar() {
 }
 
 function App() {
-	return (
+    return (
         <ThemeProvider theme={theme}>
             <Router>
                 <div className="App">
-                    <NavigationBar/>
-                    <Routes/>
+                    <NavigationBar />
+                    <Routes />
                 </div>
             </Router>
         </ThemeProvider>
