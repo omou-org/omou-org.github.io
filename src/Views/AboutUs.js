e0bcb28c638e81f9d9e8064bdefe3983636bebe4
Calvin Fronda <cfronda209@gmail.com>
2020-04-05 18:00:09 -0700

    finished about-us

--- src/Views/AboutUs.js
+++ src/Views/AboutUs.js
@@ -1,25 +1,47 @@
-import React, { useState, useMe } from "react";
-import { Container, Grid, Paper, Typography, GridList } from '@material-ui/core'
+import React from "react";
+import { Container, Grid, Typography, Box } from '@material-ui/core'
 import { Users } from "./Users/Users"
+import { makeStyles } from '@material-ui/core/styles';
 import UserCards from "./Users/UserCards"
 
+const useStyles = makeStyles({
+	roleTitle: {
+		margin: "50px 0 50px 0",
+		color: "#635b5c" //grey
+	},
+	meetTeam: {
+		margin: "75px 0 50px 0",
+	},
+	aboutUsContainer: {
+		padding: "7%",
+	},
+	aboutUs: {
+		margin: "50px 0 50px 0",
+	},
+	meetTeamPicture: {
+		width: "100%",
+		height: "auto",
+		marginTop: "20%"
+	},
 
+});
 
 const AboutUs = () => {
-
+	const classes = useStyles()
 
 	return (
 		<>
-			<div style={{ backgroundColor: "#43B5DA", color: "white" }}>
-				<Container container maxWidth="lg" className="about-us-description" >
-					<Grid container>
-						<Grid item xs={8} >
-							<Typography variant="h3" align="left"> About Us</Typography>
+			<div className={classes.aboutUsContainer} style={{ backgroundColor: "#43B5DA", color: "white" }}>
+				<Container maxWidth="lg"  >
+					<Grid container spacing={4}>
+						<Grid item md={7}>
+							<Typography className={classes.aboutUs} variant="h1" align="left"> About Us</Typography>
 							<Typography variant="body1" align="left">
 								We’re Omou. We’re born out of a desire to create digital spaces for tutoring
 								centers to grow and better connect their community of learners, parents, and teachers.
 								We were founded by former tutors and students who recognize vast accessibility of content and connectivity
-						opportunities enhancing the learning and teaching experiences.</Typography>
+								opportunities enhancing the learning and teaching experiences.
+								</Typography>
 							<br />
 							<Typography variant="body1" align="left">
 								While not a direct replacement for physical classrooms, we at Omou believe digital
@@ -31,79 +53,83 @@ const AboutUs = () => {
 								We at Omou want to remove all physical barriers that prevent students, parents, and teachers from connecting with a tutoring center’s community.
 							In the end, all that separates you and your tutoring community is just 1 click away.
 						</Typography>
+						</Grid>
+
+						<Grid item xs>
+							<Box component="div" >
+								<img className={classes.meetTeamPicture} alt="" src={require("./Users/pictures/omou.jpeg")} />
+							</Box>
+
 
 						</Grid>
-						<Grid item={4}>Picture</Grid>
 					</Grid>
 				</Container>
 			</div>
-			<Container maxWidth="xl">
-				<Typography variant="h3">Meet The Team</Typography>
-				<Container>
-					<Typography variant="h3" align="left">Leadership</Typography>
-					<Grid
-						container
-						spacing={2}
-						direction="row"
-						justify="center"
-						alignItems="flex-start"
-					>
-						{
-							Users
-								.filter((user) => user.roleID === 0)
-								.map((user) => <UserCards user={user} />
-								)
-						}
 
-					</Grid>
-					<Typography variant="h3" align="left">Front-End Engineers</Typography>
-					<Grid
-						container
-						spacing={2}
-						direction="row"
-						justify="center"
-						alignItems="flex-start"
-					>
-						{
-							Users
-								.filter((user) => user.roleID === 1)
-								.map((user) => <UserCards user={user} />
-								)
-						}
-					</Grid>
-					<Typography variant="h3" align="left">Back-End Engineers</Typography>
-					<Grid
-						container
-						spacing={2}
-						direction="row"
-						justify="center"
-						alignItems="flex-start"
-					>
-						{
-							Users
-								.filter((user) => user.roleID === 2)
-								.map((user) => <UserCards user={user} />
-								)
-						}
-					</Grid>
-					<Typography variant="h3" align="left">Product Designers</Typography>
-					<Grid
-						container
-						spacing={2}
-						direction="row"
-						justify="center"
-						alignItems="flex-start"
-					>
-						{
-							Users
-								.filter((user) => user.roleID === 3)
-								.map((user) => <UserCards user={user} />
-								)
-						}
-					</Grid>
 
-				</Container>
+
+			<Container>
+				<Typography variant="h2" align="left" className={classes.meetTeam} >Meet The Team</Typography>
+				<Typography className={classes.roleTitle} variant="h3" align="left">Leadership</Typography>
+				<Grid
+					container
+					spacing={4}
+					direction="row"
+					alignItems="flex-start"
+				>
+					{
+						Users
+							.filter((user) => user.roleID === 0)
+							.map((user) => <UserCards user={user} />
+							)
+					}
+
+				</Grid>
+				<Typography className={classes.roleTitle} variant="h3" align="left">Front-End Engineers</Typography>
+				<Grid
+					container
+					spacing={4}
+					direction="row"
+					alignItems="flex-start"
+				>
+					{
+						Users
+							.filter((user) => user.roleID === 1)
+							.map((user) => <UserCards user={user} />
+							)
+					}
+				</Grid>
+				<Typography className={classes.roleTitle} variant="h3" align="left">Back-End Engineers</Typography>
+				<Grid
+					container
+					spacing={4}
+					direction="row"
+					alignItems="flex-start"
+				>
+					{
+						Users
+							.filter((user) => user.roleID === 2)
+							.map((user) => <UserCards user={user} />
+							)
+					}
+				</Grid>
+				<Typography className={classes.roleTitle} variant="h3" align="left">Product Designers</Typography>
+				<Grid
+					container
+					spacing={4}
+					direction="row"
+					alignItems="flex-start"
+				>
+					{
+						Users
+							.filter((user) => user.roleID === 3)
+							.map((user) => <UserCards user={user} />
+							)
+					}
+				</Grid>
+
 			</Container>
+
 
 
 		</>
--- src/Views/Users/UserCards.js
+++ src/Views/Users/UserCards.js
@@ -1,22 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
+
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid'
 
 
 const useStyles = makeStyles({
     root: {
-        maxWidth: 450,
-        height: 300,
+        maxWidth: 400,
     },
     media: {
-        height: 140,
+        height: 300,
     },
 });
 
@@ -25,21 +24,23 @@ const UserCards = ({ user }) => {
     return (
 
         <Grid item sm={4}>
-            <Card className={classes.root}>
-                <CardActionArea>
+            <Card
+                className={classes.root}>
+                <CardActionArea
+                    disableTouchRipple={true}
+                    onClick={() => {
+                        window.open(user.linkedin)
+                    }}
+                >
                     <CardMedia
                         className={classes.media}
-                        image=""
+                        image={require("./pictures/" + user.picture_src)}
                         title=""
                     />
-                    {console.log(user.picture_src)}
                     <CardContent>
                         <Typography gutterBottom variant="h5" component="h2">
                             {user.name}
                         </Typography>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            LinkedIn
-          </Typography>
                         <Typography gutterBottom variant="h5" component="h2">
                             {user.role}
                         </Typography>
