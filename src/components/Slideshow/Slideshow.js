a6c4aeb9867745805db798f9454fe45d25bffffc
Wilson Lam <lin-1987@live.com>
2020-05-20 16:57:52 -0700

    modified 'featureData' by adding in a path key + slideshow now functional on all sections

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -2,11 +2,11 @@ import React from "react";
 import Carousel from "react-material-ui-carousel";
 import Items from "../Items/Items";
 
-const Slideshow = ({ images }) => {
+const Slideshow = ({ images, path }) => {
   return (
     <Carousel autoPlay animation="fade">
       {images.map((image) => (
-        <Items item={image} />
+        <Items item={image} path={path}/>
       ))}
     </Carousel>
   );
