0ebccb9aaa13e67969a6f9d26f4c76da05efb992
Wilson Lam <lin-1987@live.com>
2020-05-20 16:30:48 -0700

    first carousel working

---
+++ src/components/Items/Items.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  image: {
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+      height: "100%",
+    },
+  },
+}));
+const Items = (props) => {
+  const classes = useStyles();
+  return (
+    <Paper>
+      <img
+        src={require("../../assets/product-screenshots/standardized-registration-and-payments/" +
+          props.item)}
+        className={classes.image}
+        alt={props.item}
+        height="auto"
+        width="80%"
+      />
+    </Paper>
+  );
+};
+
+export default Items;
