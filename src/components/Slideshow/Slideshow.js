af27b9ec86be5727a854c6b6b9691b2dc9e176b8
Wilson Lam <lin-1987@live.com>
2020-05-20 17:08:14 -0700

    got rid of the bottom boxes

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -4,7 +4,7 @@ import Items from "../Items/Items";
 
 const Slideshow = ({ images, path }) => {
   return (
-    <Carousel autoPlay animation="fade">
+    <Carousel autoPlay animation="fade" indicators={false}>
       {images.map((image) => (
         <Items item={image} path={path} />
       ))}
