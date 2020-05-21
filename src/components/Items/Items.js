cd19319b12b504b6a5d12586de064a7056bfe033
Daniel Huang <danielhuang@g.ucla.edu>
2020-05-20 20:50:24 -0700

    fixed import

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { theme } from "../../theme/theme";
 import { Paper } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   image: {
