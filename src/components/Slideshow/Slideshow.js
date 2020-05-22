d9a358635342622be9df2693e9b080d54d71a3c9
Calvin Fronda <cfronda209@gmail.com>
2020-05-21 17:28:34 -0700

    carousel-fixes

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Carousel from "react-material-ui-carousel";
+import Carousel from "./Carousel";
 import Items from "../Items/Items";
 
 const Slideshow = ({ images, path }) => {
   return (
-    <Carousel autoPlay animation="fade">
+    <Carousel autoPlay animation="fade" fullHeightHover="false">
       {images.map((image) => (
         <Items item={image} path={path} />
       ))}
