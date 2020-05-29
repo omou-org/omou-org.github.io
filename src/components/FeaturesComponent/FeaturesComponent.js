beca8c34b301b0148707afa27ea6bb7d4b05d1a6
Wilson Lam <lin-1987@live.com>
2020-05-29 11:30:55 -0700

    made buttons highlight the corresponding color of the title + card

--- src/components/FeaturesComponent/FeaturesComponent.js
+++ src/components/FeaturesComponent/FeaturesComponent.js
@@ -7,10 +7,10 @@ import { NavLink } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-      marginTop: "5.75em"
+    marginTop: "5.75em",
   },
   root2: {
-      marginTop: "5.45em",
+    marginTop: "5.45em",
   },
   features: {
     fontFamily: "Roboto Slab",
@@ -26,42 +26,56 @@ const useStyles = makeStyles((theme) => ({
     top: "36.75em",
   },
   featureslist: {
-      fontFamily: "Roboto",
-      fontStyle: "normal",
-      fontWeight: 500,
-      fontSize: "1.275rem",
-      lineHeight: "1.75rem",
-      whiteSpace: "nowrap"
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: 500,
+    fontSize: "1.275rem",
+    lineHeight: "1.75rem",
+    whiteSpace: "nowrap",
   },
   buttonMargins: {
-      marginTop: ".85em",
-      marginBottom: ".85em",
+    marginTop: ".85em",
+    marginBottom: ".85em",
   },
   buttonMargins1: {
-    marginRight: "14.9em"
-},
-buttonMargins2: {
-    marginRight: "5.575em"
-},
-buttonMargins3: {
-    marginRight: "7.85em"
-},
-demoBtn: {
+    '&:hover': {
+      backgroundColor: '#1F82A1',
+  },
+  },
+  buttonMargins2: {
+    marginRight: "14.9em",
+    '&:hover': {
+      backgroundColor: '#599349',
+  },
+  },
+  buttonMargins3: {
+    marginRight: "5.575em",
+    '&:hover': {
+      backgroundColor: '#F1974A',
+  },
+  },
+  buttonMargins4: {
+    marginRight: "7.85em",
+    '&:hover': {
+      backgroundColor: '#CE6764',
+  },
+  },
+  demoBtn: {
     color: "#ffffff",
     marginRight: "11.625em",
     padding: ".5em 4em",
-},
-listMargin: {
-  marginTop: "2.5625em"
-}
+  },
+  listMargin: {
+    marginTop: "2.5625em",
+  },
 }));
 
-const FeaturesComponent = ({ click, featuresArr}) => {
+const FeaturesComponent = ({ click, featuresArr }) => {
   const classes = useStyles();
-  const handleClick = e => {
-    const { id } = e.currentTarget.dataset
+  const handleClick = (e) => {
+    const { id } = e.currentTarget.dataset;
     click(id);
-  }
+  };
 
   return (
     <Grid container className={classes.root}>
@@ -72,36 +86,60 @@ const FeaturesComponent = ({ click, featuresArr}) => {
         <span className={classes.line}></span>
       </Grid>
       <Grid item xs={12} className={classes.listMargin}>
-        <Button className={classes.buttonMargins} data-id="1" onClick={handleClick}>
-  <Typography className={classes.featureslist}>{featuresArr[0]["1"].title}</Typography>
+        <Button
+          className={`${classes.buttonMargins} ${classes.buttonMargins1}`}
+          data-id="1"
+          onClick={handleClick}
+        >
+          <Typography className={classes.featureslist}>
+            {featuresArr[0]["1"].title}
+          </Typography>
         </Button>
       </Grid>
       <Grid item xs={12}>
-        <Button className={`${classes.buttonMargins} ${classes.buttonMargins1}`} data-id="2" onClick={handleClick}>
-  <Typography className={classes.featureslist}>{featuresArr[1]["2"].title}</Typography>
+        <Button
+          className={`${classes.buttonMargins} ${classes.buttonMargins2}`}
+          data-id="2"
+          onClick={handleClick}
+        >
+          <Typography className={classes.featureslist}>
+            {featuresArr[1]["2"].title}
+          </Typography>
         </Button>
       </Grid>
       <Grid item xs={12}>
-        <Button className={`${classes.buttonMargins} ${classes.buttonMargins2}`} data-id="3" onClick={handleClick}>
-  <Typography className={classes.featureslist}>{featuresArr[2]["3"].title}</Typography>
+        <Button
+          className={`${classes.buttonMargins} ${classes.buttonMargins3}`}
+          data-id="3"
+          onClick={handleClick}
+        >
+          <Typography className={classes.featureslist}>
+            {featuresArr[2]["3"].title}
+          </Typography>
         </Button>
       </Grid>
       <Grid item xs={12}>
-        <Button className={`${classes.buttonMargins} ${classes.buttonMargins3}`} data-id="4" onClick={handleClick}>
-  <Typography className={classes.featureslist}>{featuresArr[3]["4"].title}</Typography>
+        <Button
+          className={`${classes.buttonMargins} ${classes.buttonMargins4}`}
+          data-id="4"
+          onClick={handleClick}
+        >
+          <Typography className={classes.featureslist}>
+            {featuresArr[3]["4"].title}
+          </Typography>
         </Button>
       </Grid>
       <Grid item xs={12} className={classes.root2}>
-      <NavLink to="/contact-us" style={{ textDecoration: "none" }}>
-        <Button
-          variant="contained"
-          size="large"
-          color="primary"
-          className={classes.demoBtn}
-        >
-          Request Demo
-        </Button>
-      </NavLink>
+        <NavLink to="/contact-us" style={{ textDecoration: "none" }}>
+          <Button
+            variant="contained"
+            size="large"
+            color="primary"
+            className={classes.demoBtn}
+          >
+            Request Demo
+          </Button>
+        </NavLink>
       </Grid>
     </Grid>
   );
