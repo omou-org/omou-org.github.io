4316e833da4028db7476556ce2fc18d19822cde3
Calvin Fronda <cfronda209@gmail.com>
2020-05-30 16:19:05 -0700

    updated home

--- src/routes.js
+++ src/routes.js
@@ -8,10 +8,11 @@ import AboutUs from "./Views/AboutUs";
 import ContactUs from "./Views/ContactUs";
 import Home from "./Views/Home";
 import MeetTeam from "./Views/MeetTeam";
-
+import Home2 from "./Views/Home2"
 export function Routes() {
 	return (
 		<Switch>
+			<Route path="/home2" component={Home2} />
 			<Route path="/meet-team" component={MeetTeam} />
 			<Route path="/features" component={Features} />
 			<Route path="/about-us" component={AboutUs} />
