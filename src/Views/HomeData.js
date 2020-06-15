8749765aa769c51d22a79c1877053309610706ad
Calvin Fronda <cfronda209@gmail.com>
2020-06-14 20:40:25 -0700

    responsive homepage

---
+++ src/Views/HomeData.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const HomeData = {
+    header: {
+        title: "Don’t just digitize the way your tutoring center runs -",
+        transformText: "transform ",
+        textAfterTransform: "it",
+        bottomText: "Omou is a tutoring management service designed for remote-learning."
+    },
+    content: {
+        title1: "Modernize Administrative Processes",
+        description1: "Ditch the traditional ways of scheduling tutoring classes over the phone or processing payments with cash or check. Omou will help alleviate time-consuming administrative tasks and reduce human errors by providing our customers with a simple interface for the single source of truth.",
+        title2: "Elevate Communication with Parents",
+        description2: "Miscommunication between parents and tutoring centers are often caused by disparate systems with conflicting information. Omou provides a digital central hub for parents, students, and staff alike to access personal information such as automated student progress reports, class schedules, and payment confirmations.(Comming Soon)",
+        title3: "Set Up Students For Online Success",
+        description3: "Remote learning is the future. Remove the friction of setting up and configuring tools for online tutoring. With the ability to create on-demand tutorials and recommended student activities built into Omou, online tutoring is now seamless and accessible for everyone involved.(Comming Soon)",
+    }
+}
+
