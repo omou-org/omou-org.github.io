cdc6e93bdb9c14dc7f43b9aae22df4902de70981
Calvin Fronda <cfronda209@gmail.com>
2020-11-14 20:28:13 -0800

    update

---
+++ src/components/homepage/ContentSection.js
@@ -0,0 +1,152 @@
+import React from "react";
+import {makeStyles} from "@material-ui/core/styles"
+import {Grid, Box, Container, Typography, Button} from "@material-ui/core"
+import wave from "./homeimages/wave.svg"
+import { primaryColor } from "../../theme/colors";
+
+
+const useStyles = makeStyles({
+    image: {
+        width: "500px",
+        height: "500px",
+        backgroundColor: "black",
+        
+        
+    },
+    contentContainer: {
+       height: "800px",
+       paddingTop: "10em"
+    },
+    rightContent: {
+        paddingTop: "2vh"
+        
+    },
+    contactUsButton: {
+        background: primaryColor,
+        color: "white",
+        width: "472px",
+        height: "64px",
+        borderRadius: "20px",
+        fontSize: "18px",
+        fontWeight: "bold",
+        filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))",
+        '&:hover': {
+            backgroundColor: "#A0A0A0",
+            color: '#FFF'
+    },
+    }
+})
+
+
+
+
+const ContentSection = ({image,title,description,learnMore,isRight, waveImage, displayContactUsButton}) => {
+    const classes = useStyles();
+   
+
+
+
+    return (
+
+        <>
+  {waveImage && <img src={wave} alt="wave" style={{width: "100%"}}/>}
+            
+  {isRight ? 
+  <>
+
+    <Grid container justify="space-around" className={classes.contentContainer}>
+         
+            
+            <section >
+
+                <Box  className={classes.image}   >
+
+
+                </Box>
+
+
+            </section>
+
+            <Grid xs={4}>
+                <section className={classes.rightContent}>
+                    <Typography variant="h4">
+                       {title}
+                        </Typography>
+                
+                        <Typography variant="h5" style={{paddingTop: "5vh"}}>
+                       {description}
+                        </Typography>
+                
+                        <Typography variant="h4" style={{paddingTop: "15vh"}}>
+                            Learn More
+                        </Typography>
+                        
+                </section>
+                </Grid>
+            </Grid>
+     
+        </>
+            : 
+            <>
+            
+        
+            <Grid container justify="space-around" className={classes.contentContainer}>
+            <Grid xs={4}>
+                <section className={classes.rightContent}>
+                    <Typography variant="h4">
+                       {title}
+                        </Typography>
+                
+                        <Typography variant="h5" style={{paddingTop: "5vh"}}>
+                        {description}
+                        </Typography>
+                {displayContactUsButton ? 
+                <Box style={{paddingTop: "15vh"}}>
+                        <Button
+                            className={classes.contactUsButton}
+                        >
+                        Contact Us
+                </Button> 
+                </Box>
+                                    
+                                    
+                                    
+                                    :
+                <Typography variant="h4" style={{paddingTop: "15vh"}}>
+                Learn More
+            </Typography>
+                }
+                        
+                        
+                </section>
+
+            </Grid>
+
+            <section >
+
+                <Box  className={classes.image}   >
+
+
+                </Box>
+
+
+            </section>
+
+          
+            </Grid>
+        </>
+            
+            
+           
+            }
+   </>
+    
+    )
+
+
+
+}
+
+
+
+export default ContentSection;
--- src/components/homepage/Home.js
+++ src/components/homepage/Home.js
@@ -1,67 +1,88 @@
 import React from "react";
-import {Grid, Typography, Box} from "@material-ui/core"
+import {Grid, Typography, Box, TextField} from "@material-ui/core"
 import {makeStyles} from "@material-ui/core/styles"
-import heroImage from "./homeimages/heroImage.svg"
+import people from "./homeimages/people.svg"
+import ContentSection from "./ContentSection"
+import ContactUsForm from "./ContactUsForm"
+import homePageData from "./homePageData.json"
+
 
 const useStyles = makeStyles(theme => ({
     "homepageBanner": {
         "width": "100%",
         "textAlign": "center",
         "color": "black",
-        "fontSize": "30px",
+     
     },
     "homepageText": {
-        marginLeft: "50%",
+        
+        marginLeft: "20vh",
+        [theme.breakpoints.down("md")]: {
+            fontSize: "3vh",
+        },
         [theme.breakpoints.down("sm")]: {
             marginLeft: "0",
-   
         }
-
-        
     },
     "homepageSubText": {
-        paddingTop: "2%",
-        marginLeft: "50%",
+       
+        marginLeft: "20vh",
         [theme.breakpoints.down("sm")]: {
             marginLeft: "0",
         }
-    }
+    },
+  
 }));
-
 
 const Home =() => {
 const classes = useStyles()
     return (
-        <Grid
-            container
-            >
+        <Grid container >
             <Grid 
-            item 
-            xs={12}
+             xs={12}
              container 
              justify="space-between"
              direction="row"
              alignItems="center"
              className={classes.homepageBanner} >
-                <Grid item sm={8} lg={6}>
-                    <Typography variant="h4" className={classes.homepageText}>
+                <Grid item sm={8} lg={7}>
+                    <Typography variant="h2" align="left" className={classes.homepageText}>
                         Don't just digitize the way your tutoring center runs - Transform it
                     </Typography>
                 
-                     <Typography className={classes.homepageSubText}> 
+                     <Typography className={classes.homepageSubText} align="left"> 
                     Omou is a tutoring management service designed for remote-learning.
                 </Typography>
                 </Grid>
-
-
-
                 <Grid item  alignContent="right">
-                    <Box display={{ xs: 'block', sm: 'none', md: 'block' }}>
-                    <img src={heroImage} alt="hero"/>
+                    <Box display={{ xs: 'block', sm: 'none', md: 'block' }}  >
+                    <img src={people} alt="hero" style={{verticalAlign:"top" }}/>
                     </Box>
                 </Grid>
             </Grid>
-        </Grid>
+
+        {homePageData.map(({image, title, description, learnMore , isRight, waveImage,displayContactUsButton}) => (
+            
+        <ContentSection  
+            image={image}
+            title={title}
+            description={description}
+            learnMore={learnMore}
+            isRight={isRight}
+            waveImage={waveImage}
+            displayContactUsButton={displayContactUsButton}
+        />
+     
+        ))}
+       
+
+       < ContactUsForm/>
+       
+       </Grid>
+
+
+
+
     )
 }
 
