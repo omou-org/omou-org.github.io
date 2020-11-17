1084ff1d30eff72df835ed84bc386fa8a6bb9912
Calvin Fronda <cfronda209@gmail.com>
2020-11-16 20:07:33 -0800

    homepage-done

--- src/components/homepage/ContactUsForm.js
+++ src/components/homepage/ContactUsForm.js
@@ -1,99 +1,139 @@
-import React from "react";
-import {Grid, Typography, TextField} from "@material-ui/core";
-import {makeStyles} from "@material-ui/core/styles"
+import React from 'react';
+import { Grid, Typography, TextField, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { primaryColor } from '../../theme/colors';
+import bottomWave from './homeimages/bottomWave.svg';
 
-const useStyles = makeStyles( (theme) => ({
+const useStyles = makeStyles((theme) => ({
     contactUsForm: {
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: "space-evenly",
+        justifyContent: 'space-evenly',
         margin: theme.spacing(10),
+        width: '50%',
     },
-    contactUsContainer :{
-        
+    contactUsContainer: {},
+    namePhoneInput: {
+        width: '200%',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        fontWeight: 500,
+        borderRadius: '30px',
     },
-    formInput: {
-        width: "300px"
-    }
-}))
-
-
+    emailInput: {
+        borderRadius: '30px',
+    },
+    inputText: {
+        paddingBottom: '20px',
+        fontSize: '22px',
+        fontWeight: '300',
+    },
+    emailInputText: {
+        paddingBottom: '20px',
+        paddingTop: '20px',
+        fontSize: '22px',
+        fontWeight: '300',
+    },
+    learnMoreButton: {
+        width: '166px',
+        height: '62px',
+        borderRadius: '20px',
+        backgroundColor: primaryColor,
+        color: 'white',
+    },
+}));
 
 const ContactUsForm = () => {
-const classes = useStyles();
+    const classes = useStyles();
 
     return (
-        <Grid 
-        container
-        justify="center"
-        direction="row"
-        className={classes.contactUsContainer}
->
-        
-            <Grid item  >
-                <Typography variant="h3">BOOK A DEMO</Typography>
-
-            </Grid>
-            <Grid 
+        <Grid
             container
-            direction="row"
             justify="center"
-             alignItems="center"
-            >
-            <Grid item xs={12}>
-            <form className={classes.contactUsForm}>
+            direction="row"
+            className={classes.contactUsContainer}
+        >
+            <Grid item>
+                <Typography variant="h3">BOOK A DEMO</Typography>
+            </Grid>
             <Grid
-  container
-  direction="row"
-  justify="center"
-  alignItems="center"
-  style={{textAlign: "center"}}>
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+            >
+                <form className={classes.contactUsForm}>
+                    <Grid
+                        container
+                        direction="row"
+                        justify="center"
+                        alignItems="center"
+                        spacing={1}
+                    >
+                        <Grid item xs={6}>
+                            <Typography className={classes.inputText}>
+                                FULL NAME
+                            </Typography>
 
-            
-                <Grid item xs={6}>
-                    <Typography>Name</Typography>
-                    <TextField 
-                     required
-                     variant="outlined" 
-                     id="name"
-                     className={classes.formInput}
-                     />
-                </Grid>
-        
-                <Grid item xs={6}>
-                <Typography>Phone</Typography>
-                    <TextField 
-                    required
-                    variant="outlined" 
-                     id="standard-required"  />
-                </Grid >
+                            <TextField
+                                required
+                                variant="outlined"
+                                id="name"
+                                InputProps={{
+                                    classes: {
+                                        root: classes.namePhoneInput,
+                                    },
+                                }}
+                            />
+                        </Grid>
 
-                <Grid item xs={6}  style={{textAlign: "center"}}>
-            <Typography>Email</Typography>
-            <TextField 
-            required
-            variant="outlined"  
-            fullWidth
-            id="standard-required"  />
-        </Grid>      
-        <Grid item xs={6} >
-        
-        </Grid>      
+                        <Grid item xs={6}>
+                            <Typography className={classes.inputText}>
+                                PHONE NUMBER
+                            </Typography>
+                            <TextField
+                                required
+                                variant="outlined"
+                                id="standard-required"
+                                InputProps={{
+                                    classes: {
+                                        root: classes.namePhoneInput,
+                                    },
+                                }}
+                            />
+                        </Grid>
 
-                </Grid>
-     
-        </form>    
-        </Grid>
+                        <Grid item xs={12}>
+                            <Typography className={classes.emailInputText}>
+                                EMAIL
+                            </Typography>
 
-</Grid>  
+                            <TextField
+                                required
+                                variant="outlined"
+                                fullWidth
+                                id="standard-required"
+                                InputProps={{
+                                    classes: {
+                                        root: classes.emailInput,
+                                    },
+                                }}
+                            />
+                        </Grid>
                     </Grid>
+                </form>
+            </Grid>
 
-   
-
-
-    )
-}
-
-
+            <Grid container justify="center" direction="row">
+                <Grid item>
+                    <Button className={classes.learnMoreButton}>
+                        Learn more
+                    </Button>
+                </Grid>
+            </Grid>
+            <img src={bottomWave} alt="wave2" style={{ width: '100%' }} />
+        </Grid>
+    );
+};
 
 export default ContactUsForm;
