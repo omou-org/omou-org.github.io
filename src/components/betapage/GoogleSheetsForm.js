e8c1385eaedbb7214225a5fac1afeaafcaeb9b27
Calvin Fronda <cfronda209@gmail.com>
2020-11-27 13:38:21 -0800

    removed api

--- src/components/betapage/GoogleSheetsForm.js
+++ src/components/betapage/GoogleSheetsForm.js
@@ -1,18 +1,6 @@
 import React, { useEffect } from 'react';
 import { Grid, Typography, TextField, Button } from '@material-ui/core';
 
-const SPREADSHEET_ID = '1VZwL2O9_QpoOOKXU8WyVjDRO7wWJzQx5ylizdtM_UJE';
-const CLIENT_ID =
-    '281034201526-kk4ve7gtbj93krlsmb38q94jnbbmj78i.apps.googleusercontent.com';
-const API_KEY = 'AIzaSyB9SamxnXpDQC7yk5qVHTOiQly4WVKQthU';
-
-//'GuAWocZ28OiVB09JJPh4I8Nj';
-const SCOPE = 'https://www.googleapis.com/auth/spreadsheets';
-
-//play ground
-
-const ACCESS_TOKEN = 'ya29.a0AfH6SMBjbowJMt7DALWb26Tc_pJDIjLD6sL7d';
-
 const GoogleSheetsForm = () => {
     const updateSheetValues = () => {
         fetch(
