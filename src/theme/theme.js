66daff9f9d5a960fae9ea6a56137c7a4d49c229b
Calvin Fronda <cfronda209@gmail.com>
2020-12-09 21:43:53 -0800

    changed logo size

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,63 +1,63 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import {primaryColor} from "./colors";
+import { primaryColor } from './colors';
 
 export const appBarTheme = createMuiTheme({
-	palette: {
-		primary: {main: "#ffffff"},
-		secondary: {main: "#a6a6a6"},
-		background: {
-			main: "#ffffff",
-			default: "#ffffff",
-		},
-	},
-	typography:{
-		h3:{
-			fontFamily: "Montserrat",
-			fontWeight:700,
-			color: primaryColor,
-		}
-	},
-	overrides:{
-		MuiAppBar:{
-			root:{
-				boxShadow: "none",
-			}
-		}
-	}
+    palette: {
+        primary: { main: '#ffffff' },
+        secondary: { main: '#a6a6a6' },
+        background: {
+            main: '#ffffff',
+            default: '#ffffff',
+        },
+    },
+    typography: {
+        h4: {
+            fontFamily: 'Montserrat',
+            fontWeight: 700,
+            color: primaryColor,
+        },
+    },
+    overrides: {
+        MuiAppBar: {
+            root: {
+                boxShadow: 'none',
+            },
+        },
+    },
 });
 
 export const theme = createMuiTheme({
-	palette: {
-		primary: {main: primaryColor},
-		secondary: {main: "#a6a6a6"},
-		background: {
-			main: "#ffffff",
-			default: "#ffffff",
-		},
-	},
-	typography:{
-		h1:{
-			fontFamily: "Montserrat",
-			fontWeight:700,
-		},
-		h2:{
-			fontFamily: "Montserrat",
-			fontWeight:700,
-		},
-		h3:{
-			fontFamily: "Montserrat",
-			fontWeight:700,
-		},
-		h4:{
-			fontFamily: "Montserrat",
-			fontWeight:700,
-		}
-	},
-	overrides:{
-		MuiButton:{
-			containedPrimary: {
-				color:"white",
-			},
-		}
-	}
-})
+    palette: {
+        primary: { main: primaryColor },
+        secondary: { main: '#a6a6a6' },
+        background: {
+            main: '#ffffff',
+            default: '#ffffff',
+        },
+    },
+    typography: {
+        h1: {
+            fontFamily: 'Montserrat',
+            fontWeight: 700,
+        },
+        h2: {
+            fontFamily: 'Montserrat',
+            fontWeight: 700,
+        },
+        h3: {
+            fontFamily: 'Montserrat',
+            fontWeight: 700,
+        },
+        h4: {
+            fontFamily: 'Montserrat',
+            fontWeight: 700,
+        },
+    },
+    overrides: {
+        MuiButton: {
+            containedPrimary: {
+                color: 'white',
+            },
+        },
+    },
+});
